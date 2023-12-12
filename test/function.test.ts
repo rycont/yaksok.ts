8af496dc3d08280a044539c9dfafcb2583c28fef
정한 Rycont <35295182+rycont@users.noreply.github.com>
2023-12-12 11:39:07 +0000

    test: add tests for function

---
+++ test/function.test.ts
@@ -0,0 +1,94 @@
+import { assertEquals, assertIsError, unreachable } from 'assert'
+import { run } from '../runtime.ts'
+import { parse } from '../parser.ts'
+import { tokenizer } from '../tokenizer.ts'
+import { preprocessor } from '../preprocessor.ts'
+import {
+    BlockPiece,
+    DeclareVariablePiece,
+    EvaluatablePiece,
+    FunctionDeclarationPiece,
+    FunctionInvokePiece,
+    KeywordPiece,
+    NumberPiece,
+    VariablePiece,
+} from '../piece/index.ts'
+import { CallFrame, Scope } from '../scope.ts'
+import { YaksokError } from '../errors.ts'
+
+Deno.test('Function that returns value', () => {
+    const code = `
+약속 가"와" 나"를 더하기"
+    결과: 가 + 나
+
+더한결과: 10와 20를 더하기
+`
+    const result = run(parse(tokenizer(preprocessor(code))))
+    assertEquals(result.getVariable('더한결과'), new NumberPiece(30))
+})
+
+Deno.test('Function that returns nothing', () => {
+    const code = `
+약속 가"와" 나"를 더하기"
+    아무것도아닌거: 가 + 나
+
+더한결과: 10와 20를 더하기
+`
+    const result = run(parse(tokenizer(preprocessor(code))))
+    assertEquals(result.getVariable('더한결과'), new NumberPiece(0))
+})
+
+Deno.test('Function invoke argument is not evaluable', async (context) => {
+    const scope = new Scope()
+
+    await context.step('Create function', () => {
+        const testFunction = new FunctionDeclarationPiece({
+            name: '주문하기',
+            body: new BlockPiece([
+                new DeclareVariablePiece({
+                    name: new VariablePiece({
+                        name: new KeywordPiece('나이'),
+                    }),
+                    value: new NumberPiece(20),
+                }),
+                new DeclareVariablePiece({
+                    name: new VariablePiece({
+                        name: new KeywordPiece('결과'),
+                    }),
+                    value: new NumberPiece(10),
+                }),
+            ]),
+        })
+
+        testFunction.execute(scope)
+    })
+
+    await context.step('Invoke function with broken arguments', () => {
+        const functionInvokation = new FunctionInvokePiece({
+            음식: new KeywordPiece('사과') as unknown as EvaluatablePiece,
+            name: '주문하기',
+        } as unknown as Record<string, EvaluatablePiece> & { name: string })
+
+        try {
+            functionInvokation.execute(scope, new CallFrame(functionInvokation))
+            unreachable()
+        } catch (e) {
+            assertIsError(e, YaksokError)
+            assertEquals(e.name, 'NOT_EVALUABLE_EXPRESSION')
+        }
+    })
+
+    await context.step('Invoke function with no name', () => {
+        const functionInvokation = new FunctionInvokePiece({
+            음식: new KeywordPiece('사과') as unknown as EvaluatablePiece,
+        } as unknown as Record<string, EvaluatablePiece> & { name: string })
+
+        try {
+            functionInvokation.execute(scope, new CallFrame(functionInvokation))
+            unreachable()
+        } catch (e) {
+            assertIsError(e, YaksokError)
+            assertEquals(e.name, 'FUNCTION_MUST_HAVE_NAME')
+        }
+    })
+})
