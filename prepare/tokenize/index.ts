33c02243e6fa7da0d50297d1850274c2eaf10e32
정한 Rycont <35295182+rycont@users.noreply.github.com>
2023-12-25 07:24:07 +0000

    feat: add '@' to expression characters

--- prepare/tokenize/index.ts
+++ prepare/tokenize/index.ts
@@ -28,7 +28,7 @@ export class Tokenizer {
     column = 0
 
     static OPERATORS = ['+', '-', '*', '/', '>', '=', '<', '~']
-    static EXPRESSIONS = ['{', '}', ':', '[', ']', ',', '(', ')']
+    static EXPRESSIONS = ['{', '}', ':', '[', ']', ',', '(', ')', '@']
 
     constructor(code: string, private disablePosition = false) {
         this.chars = this.preprocess(code)
