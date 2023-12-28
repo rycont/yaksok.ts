c1de2479bc7b8237a6e647c5255a72dc7adca83c
정한 Rycont <35295182+rycont@users.noreply.github.com>
2023-12-28 11:26:12 +0000

    chore: remove local environment files

--- runtest.ts
+++ runtest.ts
@@ -1,17 +1,14 @@
 import { yaksok } from './index.ts'
 
-let stderr = ''
-
-yaksok(
-    {
-        main: `
+yaksok({
+    main: `
 보드_시리얼: "1032"
 
 만약 @아두이노 모델명 = "Arduino Uno" 이면
     "아두이노 모델명이 맞습니다." 보여주기
     @아두이노 보드_시리얼 버전 보여주기
 `,
-        아두이노: `
+    아두이노: `
 약속 시리얼 "버전"
     만약 시리얼 = "1032" 이면
         결과: "1.0.0"
@@ -21,14 +18,6 @@ yaksok(
         아니면
             결과: "UNKNOWN"
 
-    "지민"/2
-
 모델명: "Arduino Uno"
 `,
-    },
-    {
-        stderr: (str) => (stderr += str + '\n'),
-    },
-)
-
-console.dir(JSON.stringify(stderr))
+})
