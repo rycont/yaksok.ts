31d68f7d80327ad2169c472297e85a18ec83b126
정한 Rycont <35295182+rycont@users.noreply.github.com>
2023-12-30 00:25:55 +0000

    docs: remove unsupport message for elseif

--- runtest.ts
+++ runtest.ts
@@ -1,12 +1,23 @@
-import { yaksok } from './index.ts'
+// import { yaksok } from './index.ts'
+import { yaksok } from 'https://deno.land/x/yaksok@beta-13/index.ts'
 
-const code = `
-만약 "1031" = "1032" 이면
-    "1.0.1" 보여주기
-아니면 만약 "1033" = "1033" 이면
-    "1.0.2" 보여주기
-아니면
-    "1.0.3" 보여주기
-`
+yaksok({
+    main: `
+보드_시리얼: "1033"
 
-yaksok(code)
+만약 @아두이노 모델명 = "Arduino Uno" 이면
+    "아두이노 모델명이 맞습니다." 보여주기
+    @아두이노 보드_시리얼 버전 보여주기
+`,
+    아두이노: `
+약속 시리얼 "버전"
+    만약 시리얼 = "1032" 이면
+        결과: "1.0.0"
+    아니면 만약 시리얼 = "1033" 이면
+        결과: "1.0.1"
+    아니면
+        결과: "UNKNOWN"
+
+모델명: "Arduino Uno"
+`,
+})
