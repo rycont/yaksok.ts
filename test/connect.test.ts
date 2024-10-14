ecf336bae75131ed2483ad4264a54da4c85f7730
rycont <rycont@outlook.kr>
2024-10-14 15:20:35 +0900

    test: add connect mentioning running tests

--- test/connect.test.ts
+++ test/connect.test.ts
@@ -58,3 +58,37 @@ Deno.test('연결 문법을 사용하여 자바스크립트 함수 호출', () =
         '10개는 너무 적으니, 7개 더 먹어서 17개 먹어야 겠어요.\n',
     )
 })
+
+Deno.test('다른 파일에 있는 연결 호출', () => {
+    let output = ''
+    yaksok(
+        {
+            유틸: `번역(QuickJS), (질문) 물어보기
+***
+    return "황선형"
+***`,
+            main: `
+(@유틸 ("이름이 뭐에요?") 물어보기) 보여주기
+            `,
+        },
+        {
+            runFFI(runtime, bodyCode, args) {
+                if (runtime === 'QuickJS') {
+                    const result = quickJS.run(bodyCode, args)
+                    if (!result) {
+                        throw new Error('Result is null')
+                    }
+
+                    return result
+                }
+
+                throw new Error(`Unknown runtime: ${runtime}`)
+            },
+            stdout(value) {
+                output += value + '\n'
+            },
+        },
+    )
+
+    assertEquals(output, '황선형\n')
+})
