8b21f0971396b4ee9bb4f450bf117f5a85b13cc8
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-11-03 06:21:03 +0000

    chore: initialize vitepress

--- create-docs.ts
+++ create-docs.ts
@@ -1,17 +1,68 @@
 import typedoc from 'npm:typedoc@0.26.11'
 
 const app = await typedoc.Application.bootstrapWithPlugins({
-    entryPoints: ['./index.ts'],
+    entryPoints: ['./src/index.ts'],
     name: 'yaksok.ts',
 })
 
-Deno.watchFs('./*', {
+const outputDir = 'docs/api'
+const VALID_OPERATIONS = ['create', 'modify', 'rename', 'remove']
+
+const isWatch = Deno.args.includes('--watch')
+const CWD = Deno.cwd()
+
+async function buildMarkdownDocs() {
+    const project = await app.convert()
+    if (project) {
+        await app.generateDocs(project, outputDir)
+    }
+}
+
+await buildMarkdownDocs()
+
+if (!isWatch) {
+    Deno.exit(0)
+}
+
+console.log('Waiting for file changes...')
+
+const watcher = Deno.watchFs('./src', {
     recursive: true,
 })
 
-const project = await app.convert()
-const outputDir = 'docs'
+for await (const event of watcher) {
+    const type = event.kind
+    const isValidOperation = VALID_OPERATIONS.includes(type)
 
-if (project) {
-    await app.generateDocs(project, outputDir)
+    const paths = event.paths.map(clarifyPath)
+    const message = `[${type}] ${paths.join(', ')}`
+
+    if (!isValidOperation) {
+        continue
+    }
+
+    console.log(message)
+    buildMarkdownDocs()
+}
+
+function clarifyPath(path: string) {
+    if (path.startsWith(CWD)) {
+        path = path.slice(CWD.length)
+    }
+
+    while (true) {
+        if (path.startsWith('/')) {
+            path = path.slice(1)
+            continue
+        }
+
+        if (path.startsWith('./')) {
+            path = path.slice(1)
+            continue
+        }
+
+        break
+    }
+
+    return path
 }
