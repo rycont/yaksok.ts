8cce4ebb81a3d29064e0579aa9c87da15e538e72
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-11-10 07:41:58 +0000

    chore: update runtest.ts

--- runtest.ts
+++ runtest.ts
@@ -1,25 +1,41 @@
 import { FunctionParams } from './src/mod.ts'
-import { StringValue } from './src/mod.ts'
 import { NumberValue } from './src/mod.ts'
 import { yaksok } from './src/mod.ts'
 
-function runFFI(runtime: string, code: string, params: FunctionParams) {
-    const payload = JSON.parse(code)
-
-    if (payload.action === 'getUserAgent') {
-        return new StringValue(navigator.userAgent)
-    }
-}
-
-const code = `
-번역(JavaScript), 브라우저의 UserAgent 가져오기
+yaksok(
+    `
+번역(JavaScript), (A)와 (B) 사이 랜덤 수
 ***
-{
-    "action": "getUserAgent"
-}
+    return Math.floor(Math.random() * (B - A) + A)
 ***
 
-브라우저의 UserAgent 가져오기 보여주기
-`
+(1)와 (10) 사이 랜덤 수 보여주기
+`,
+    {
+        runFFI: (runtime, code, params) => {
+            if (runtime !== 'JavaScript') {
+                throw new Error('지원하지 않는 런타임입니다')
+            }
 
-yaksok(code, { runFFI })
+            const runnableCode = buildCodeFromCodeAndParams(code, params)
+            const resultInJS = eval(runnableCode)
+
+            if (typeof resultInJS !== 'number') {
+                throw new Error('결과값은 숫자여야 합니다')
+            }
+
+            return new NumberValue(resultInJS)
+        },
+    },
+)
+
+function buildCodeFromCodeAndParams(code: string, params: FunctionParams) {
+    const paramNames = Object.keys(params)
+    const paramsInJS = Object.fromEntries(
+        Object.entries(params).map(([key, value]) => [key, value.value]),
+    )
+
+    return `((${paramNames.join(', ')}) => {${code}})(${Object.values(
+        paramsInJS,
+    ).join(', ')})`
+}
