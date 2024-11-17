a0cf356f2ba3727564b59ce4c7cf2eacc4e50aa0
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-11-17 08:39:10 +0000

    docs: apply API changes to document

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,12 @@
+export { CodeFile } from './type/code-file.ts'
 export { yaksok, Runtime } from './runtime/index.ts'
-export type { RuntimeConfig } from './runtime/runtime-config.ts'
 
 export { Scope } from './executer/scope.ts'
 export * from './node/index.ts'
 
 export { tokenize } from './prepare/tokenize/index.ts'
 
+export type { RuntimeConfig } from './runtime/runtime-config.ts'
 export type { FunctionParams } from './constant/type.ts'
 export type { FEATURE_FLAG } from './constant/feature-flags.ts'
 export type * from './error/index.ts'
