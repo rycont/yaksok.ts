907cf6c1b4dea1e9f6781a4ae0d429324198cc19
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-06 08:57:48 +0000

    chore: bump package versions to 0.1.19-alpha.1

--- runtest.ts
+++ runtest.ts
@@ -1,25 +1,27 @@
-import { QuickJS } from '@yaksok-ts/quickjs'
 import { yaksok } from '@yaksok-ts/core'
+import { NumberValue } from './src/node/primitive.ts'
 
-const quickjs = new QuickJS({
-    prompt,
-})
-await quickjs.init()
+console.time('실행')
 
-yaksok(
+await yaksok(
     `
-번역(QuickJS), (질문) 물어보기
+번역(Runtime), (숫자)초 기다리기
 ***
-    return prompt(질문)
+wait
 ***
 
-입력받은_이름: "이름이 뭐에요?" 물어보기
-입력받은_이름 + "님 안녕하세요!" 보여주기
+"안녕!" 보여주기
+1초 기다리기
+"반가워!" 보여주기
 `,
     {
-        runFFI(r, code, args) {
-            const result = quickjs.run(code, args)
-            return result
+        async runFFI(_runtime, _code, args) {
+            await new Promise((ok) =>
+                setTimeout(ok, (args.숫자 as NumberValue).value * 1000),
+            )
+
+            return new NumberValue(0)
         },
     },
 )
+console.timeEnd('실행')
