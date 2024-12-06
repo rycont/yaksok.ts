20cd00239e7b6231ed24cf4b63c1ee1174f4c987
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-06 11:34:14 +0000

    docs: apply changes to document sample codes

--- runtest.ts
+++ runtest.ts
@@ -1,34 +1,18 @@
+import { yaksok } from '@yaksok-ts/core'
 import { QuickJS } from '@yaksok-ts/quickjs'
-import { yaksok, type ValueType } from '@yaksok-ts/core'
 
-const quickJS = new QuickJS()
+const quickJS = new QuickJS({
+    prompt: () => {
+        return '10'
+    },
+})
+
 await quickJS.init()
 
-const code = `
-번역(QuickJS), (최소)와 (최대) 사이의 랜덤한 수
-***
-    return Math.floor(Math.random() * (최대 - 최소 + 1)) + 최소
-***
-
-(1)와 (10) 사이의 랜덤한 수 보여주기
-`
-
-function runFFI(
-    runtime: string,
-    code: string,
-    params: Record<string, ValueType>,
-) {
-    if (runtime !== 'QuickJS') {
-        throw new Error(`Unknown runtime: ${runtime}`)
-    }
-
-    const result = quickJS.run(code, params)
-
-    if (!result) {
-        throw new Error('Result is null')
-    }
-
-    return result
-}
-
-yaksok(code, { runFFI })
+await yaksok({
+    아두이노: `
+약속, 이름
+    결과: "아두이노" / 2
+`,
+    main: '(@아두이노 이름) 보여주기',
+})
