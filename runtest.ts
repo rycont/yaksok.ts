b417d40351e940e8ff6c347a93ccd6ca6c79cd65
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-06 10:55:22 +0000

    docs: add await keyword to demo codes

--- runtest.ts
+++ runtest.ts
@@ -1,27 +1,8 @@
 import { yaksok } from '@yaksok-ts/core'
-import { NumberValue } from './src/node/primitive.ts'
 
-console.time('실행')
+const code = `
+내_이름: "영희"
+`
 
-await yaksok(
-    `
-번역(Runtime), (숫자)초 기다리기
-***
-wait
-***
-
-"안녕!" 보여주기
-1초 기다리기
-"반가워!" 보여주기
-`,
-    {
-        async runFFI(_runtime, _code, args) {
-            await new Promise((ok) =>
-                setTimeout(ok, (args.숫자 as NumberValue).value * 1000),
-            )
-
-            return new NumberValue(0)
-        },
-    },
-)
-console.timeEnd('실행')
+const result = await yaksok(code)
+console.log(result.getFileRunner().scope.getVariable('내_이름').value)
