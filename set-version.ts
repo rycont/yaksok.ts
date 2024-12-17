9577b25547b2be44232c6954e316024de7260963
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-17 11:18:14 +0000

    feat: implement version unifier

---
+++ set-version.ts
@@ -0,0 +1,14 @@
+const rootModule = JSON.parse(await Deno.readTextFile('./deno.json'))
+const subModules = rootModule.workspace
+
+for (const subModule of subModules) {
+    const subModulePath = `./${subModule}/deno.json`
+    const subModuleJson = JSON.parse(await Deno.readTextFile(subModulePath))
+
+    subModuleJson.version = rootModule.version
+
+    await Deno.writeTextFile(
+        subModulePath,
+        JSON.stringify(subModuleJson, null, 2),
+    )
+}
