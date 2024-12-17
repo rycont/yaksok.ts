e642f19670a37fd1d98eda454152479b77cfbb07
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-17 10:55:08 +0000

    fix: make test module contained to workspace

--- test/codes/index.test.ts
+++ test/codes/index.test.ts
@@ -1,14 +1,16 @@
 import { assertEquals } from 'assert'
 import { yaksok } from '../../core/mod.ts'
 
-for (const file of Deno.readDirSync('./test/codes')) {
+for (const file of Deno.readDirSync(new URL('./', import.meta.url).pathname)) {
     if (file.isFile && file.name.endsWith('.yak')) {
         Deno.test(file.name, async () => {
             let printed = ''
 
-            const code = await Deno.readTextFile(`./test/codes/${file.name}`)
+            const code = await Deno.readTextFile(
+                new URL(`./${file.name}`, import.meta.url).pathname,
+            )
             const expected = await Deno.readTextFile(
-                `./test/codes/${file.name}.out`,
+                new URL(`./${file.name}.out`, import.meta.url).pathname,
             )
 
             await yaksok(code, {
